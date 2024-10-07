cc044a4c9f2ef9d3286c14b9b5f31e63d8155383
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-07 20:45:16 +0200

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,37 @@ import "./App.css";
 
 function App() {
   const [items, setItems] = useState(data);
+  const [cartItems, setCartItems] = useState([]);
+
+  function addItemHandler(id) {
+    const newItem = items.find((item) => item.id === id);
+    setCartItems((prev) => prev.push(newItem));
+    console.log(newItem);
+  }
 
   return (
-    <>
-      <ul>
-        {items.map((item) => {
-          return items && <Item {...item} key={item.id} />;
-        })}
-      </ul>
-    </>
+    <main>
+      <section>
+        <h1 className="app__title">DESSERTS</h1>
+        <ul className="item__list">
+          {items.map((item) => {
+            return (
+              items && (
+                <Item {...item} key={item.id} addItemHandler={addItemHandler} />
+              )
+            );
+          })}
+        </ul>
+      </section>
+      <aside className="cart">
+        <h1 className="cart__counter">Your cart ({cartItems.length})</h1>
+        {cartItems.length === 0 ? (
+          <p className="cart__p">Your cart is empty</p>
+        ) : (
+          <ul className="cart__list"></ul>
+        )}
+      </aside>
+    </main>
   );
 }
 
--- src/item.jsx
+++ src/item.jsx
@@ -1,16 +1,18 @@
 function Item(props) {
-  const { id, category, name, price, amount, img } = props;
+  const { id, category, name, price, amount, img, addItemHandler } = props;
 
   return (
     <li className="list__item">
       <div className="image__container">
-        <img src={img} alt="" />
-        <button className="add__btn">Add to cart</button>
+        <img src={img} alt="" className="item__img" />
+        <button onClick={() => addItemHandler(id)} className="add__btn">
+          Add to cart
+        </button>
       </div>
       <div className="info__container">
         <h1 className="category">{category}</h1>
         <h1 className="name">{name}</h1>
-        <h1 className="price">{price}</h1>
+        <h1 className="price">${price}</h1>
       </div>
     </li>
   );
